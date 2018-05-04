01eb5bb41945241b2f90d0376aa74cc77bca5652
Xinrea <s11s@vip.qq.com>
2018-05-05 00:04:03 +0800

    Construct and Send Request

--- network.cpp
+++ network.cpp
@@ -4,7 +4,7 @@
 using std::clog;
 using std::endl;
 
-#define LOG(T) clog<<"NetworkLog: "<<T<<endl
+#define LOG(T) clog<<"NetLog: "<<T<<endl
 
 Network::Network() {
 
@@ -19,28 +19,66 @@ Network::~Network() {
 }
 
 bool Network::request(string url) {
+    /* Get Host IP Address */
     target_url = url;
-    char *host_name = get_host_name(url);
+    string host_name = get_host_name(url);
     LOG(host_name);
-    hostent *host_info = gethostbyname(host_name);
+    hostent *host_info = gethostbyname(host_name.c_str());
     in_addr host_addr;
-    //DEBUG_BEGIN
+    /* DEBUG_BEGIN */
     LOG("Resolve Host Name");
     for(auto i = host_info->h_addr_list; *i != nullptr; ++i) {
         host_addr.s_addr = *((unsigned int *)i);
         LOG(inet_ntoa(host_addr));
     }
-    //DEBGU_END
+    /* DEBGU_END */
     host_addr.s_addr = *((unsigned int *)host_info->h_addr_list[0]);
 
-    //free memory
-    delete[] host_name;
+    /* Create TCP Socket Use 80 Port (Http) */
+    int sock_to_server;
+    sockaddr_in target_server;
+    sock_to_server = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(sock_to_server < 0){
+        LOG(sock_to_server << " | Create Socket Error");
+        return false;
+    }
+    /* Init sockaddr_in */
+    memset(&target_server,0,sizeof(sockaddr));
+    target_server.sin_family = AF_INET; //IPv4
+    target_server.sin_addr.s_addr = host_addr.s_addr;
+    target_server.sin_port = htons(80);
+    /* Establish Connection */
+    int res = connect(sock_to_server,(sockaddr*)&target_server,sizeof(target_server));
+    if(res < 0){
+        LOG(res << " | Connect Error");
+        close(sock_to_server);
+        return false;
+    }
+    /* Construct Request */
+    string request_s = 
+    "GET /dynamic_svr/v1/dynamic_svr/space_history?host_uid=82389 HTTP/1.1\n\rHost: " + host_name + "\n\r" +
+    "Connection: keep-alive";
+    LOG(request_s);
+    res = send(sock_to_server,request_s.c_str(),request_s.length(),0);
+    LOG("Send Finish");
+    if(res != request_s.length()){
+        LOG(res<<"/"<<request_s.length()<<" | Send Miss");
+        close(sock_to_server);
+        return false;
+    }
+    char buffer[2000];
+    LOG("Recv Start");
+    res = recv(sock_to_server,buffer,2000,0);
+    LOG("Recv Finish");
+    LOG("\n"<<buffer);
+    /* Free Memory */
+    close(sock_to_server);
     return true;
 }
 
-// To find the domain in url
-char* Network::get_host_name(string url) {
-    char *host_name = new char[50];
+// To find the domain in url (http://xxx.xxx.com/xxx => xxx.xxx.com)
+string Network::get_host_name(const string& url) {
+    string host_name;
     int begin = 0, end = 0, count = 0;
     bool search_begin = true;
     for(int i = 0; i < url.length(); ++i) {
@@ -54,6 +92,6 @@ char* Network::get_host_name(string url) {
             break;
         }
     }
-    strcpy(host_name,url.substr(begin+1,end-begin-1).c_str());
+    host_name = url.substr(begin+1,end-begin-1);
     return host_name;
 }
--- network.h
+++ network.h
@@ -1,17 +1,28 @@
+#ifndef BILIBILIWATCHER_NETWORK_H_
+#define BILIBILIWATCHER_NETWORK_H_
+
 #include <string>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 using std::string;
 class Network {
 	private:
 		string target_url;
+		int json_length;
+		char* json_data;
 	public:
 		explicit Network();
 		Network(string url);
 		~Network();
 		bool request(string url);
+		int length() {return json_length;}
+		char* data() {return json_data;}
 	private:
-		char* get_host_name(string url);
+		string get_host_name(const string& url);
 };
+
+#endif
