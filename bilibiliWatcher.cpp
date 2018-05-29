c5fda8c1d4bdf0f9d269c897e329d55f3f720e4a
Xinrea <s11s@vip.qq.com>
2018-05-29 11:10:32 +0800

    sql connection test

--- bilibiliWatcher.cpp
+++ bilibiliWatcher.cpp
@@ -2,17 +2,31 @@
 #include "rapidjson/document.h"
 #include <mysql_driver.h>
 #include <mysql_connection.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
 
 #include <iostream>
+#include <vector>
 using namespace rapidjson;
+using namespace std;
 
 int main() {
     sql::mysql::MySQL_Driver *driver;
     sql::Connection *con;
+    sql::Statement *stat;
+    sql::ResultSet *res;
     driver = sql::mysql::get_mysql_driver_instance();
-    con = driver->connect("tcp://mimori.app:3306","bw_admin","test");
-    std::cout << "connected\n";
-    std::cout << con->isValid() << std::endl;
+    con = driver->connect("*","*","*");
+    stat = con->createStatement();
+    res = stat->executeQuery("SELECT upid FROM upinfo");
+    vector<int> uplist;
+    while(res->next()){
+        std::cout << res->getInt(1) << std::endl;
+        uplist.push_back(res->getInt(1));
+    }
+    //TODO: insert cardinfo into table by uplist
+    delete res;
+    delete stat;
     delete con;
     return 0;
 }
